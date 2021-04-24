7c3e7ddd607341192027e69494601081aa11ba47
AidoneusUzi <80789062+theuzii@users.noreply.github.com>
2021-04-24 07:48:37 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -95,6 +95,7 @@ slash.handle("etkinlik", (d) => {
 
 slash.handle("bilgi", (d) => {
   d.reply(
+    `• [/etkinlik **Kanal Adı** **Etkinlik Adı**.](<https://github.com/theuzii>)\n` +
     `• [Botu kendi sunucuna davet et.](<https://discord.com/api/oauth2/authorize?client_id=758821820924952576&permissions=1&scope=applications.commands%20bot>)\n` +
       `• [Web Sitesi Yakında.](<https://github.com/DjDeveloperr/ActivitiesBot>)\n` +
       `• [Destek sunucumuza katıl.](<https://discord.gg/y4GbJ4ha3Z>)`,
