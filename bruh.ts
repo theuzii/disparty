8d8a10dbeca8db49e9ec0d33fa8ac611f9200ead
AidoneusUzi <80789062+theuzii@users.noreply.github.com>
2021-04-25 14:25:00 +0300

    Update bruh.ts

--- bruh.ts
+++ bruh.ts
@@ -3,7 +3,7 @@ import * as slash from "https://raw.githubusercontent.com/harmonyland/harmony/ma
 // Pick up TOKEN and PUBLIC_KEY from ENV.
 slash.init({ env: true });
 
-slash.handle("invite", (d) => {
+slash.handle("bilgi", (d) => {
   d.reply(
     `• Kullanım şekli : /etkinlik <**<Kanal Adı>** **<Etkinlik Adı>**\n` +
     `• [Botu kendi sunucuna davet et.](<https://discord.com/api/oauth2/authorize?client_id=758821820924952576&permissions=1&scope=applications.commands%20bot>)\n` +
