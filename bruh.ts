6dd9c1db7d83fe41bd56f7ea927a479807e0cc0b
AidoneusUzi <80789062+theuzii@users.noreply.github.com>
2021-04-25 14:37:17 +0300

    Update bruh.ts

--- bruh.ts
+++ bruh.ts
@@ -6,7 +6,7 @@ slash.init({ env: true });
 slash.handle("bilgi", (d) => {
   d.reply(
     `• [Beni sunucuna ekle.](<https://discord.com/api/oauth2/authorize?client_id=758821820924952576&permissions=1&scope=applications.commands%20bot>),\n` +
-      `• [Destek sunucuma katıl.](<https://discord.gg/y4GbJ4ha3Z>)`
+      `• [Destek sunucuma katıl.](<https://discord.gg/y4GbJ4ha3Z>)`,
     { ephemeral: true }
   );
 });
